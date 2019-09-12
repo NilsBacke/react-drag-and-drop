53347419b2f206c76a6966d4a779463184f5c99e
Nils Backe <nils.backe@gmail.com>
2019-09-11 22:40:42 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useDrag } from 'react-dnd'
+import { ItemTypes } from './Constants'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const text = 'Sample Text'
+	const [{ opacity }, dragRef] = useDrag({
+		item: { type: ItemTypes.CARD, text },
+		collect: monitor => ({
+			opacity: monitor.isDragging() ? 0.5 : 1
+		})
+	})
+	return (
+		<div className='App'>
+			{text} ref={dragRef} style={{ opacity }}
+		</div>
+	)
 }
 
-export default App;
+export default App
